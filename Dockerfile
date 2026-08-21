f50d243689de712ccb4d60742bba4f8e834b1d96
Anael Marcos Da Silva <anaelmarcos2018@gmail.com>
2026-08-21 08:51:40 -0300

    Corrigir conexao PostgreSQL e Docker

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM php:8.2-apache
 
-RUN docker-php-ext-install mysqli
+RUN docker-php-ext-install pgsql pdo_pgsql
 
 COPY . /var/www/html/
 
 RUN chown -R www-data:www-data /var/www/html
 
 EXPOSE 80
+
+CMD ["apache2-foreground"]
--- salvar.php
+++ salvar.php
@@ -5,12 +5,14 @@ include("conexao.php");
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 
-$sql = "INSERT INTO usuarios(nome,email)
-VALUES('$nome','$email')";
+$sql = "INSERT INTO usuarios (nome, email)
+        VALUES ($1, $2)";
 
-if(mysqli_query($conexao,$sql)){
+$resultado = pg_query_params($conn, $sql, [$nome, $email]);
+
+if ($resultado) {
     echo "<h2>Usuário cadastrado com sucesso!</h2>";
-}else{
+} else {
     echo "<h2>Erro ao cadastrar usuário.</h2>";
 }
 
